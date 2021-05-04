6fad1816aa06f5c1ca22ed9fb1e54efc525d61ba
Rajat Dash <apple@apples-MacBook-Air.local>
2021-05-04 17:22:33 +0530

    dev changes

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ const App = () => {
   return (
    <Fragment>
      <h1>hello alins</h1>
+     <h1>hi there</h1>
      <video playsInline autoPlay loop muted style={{width:"20%",margin:"auto"}} className="mt-3 mb-3">
                                 <source src={VID} type="video/mp4"/>
                                 your browser does not support tag.
